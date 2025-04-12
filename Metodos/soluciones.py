4fa3c37f3d0ecde2871f01126e3c460583c230b6
Gabriel Diaz <dg22023@ues.edu.sv>
2025-04-12 13:33:12 -0600

    Version Colaborativa

--- Metodos/soluciones.py
+++ Metodos/soluciones.py
@@ -157,6 +157,63 @@ def lu_decomposition(A, b, tol=1e-12):
     return x
 
 
+#Metodo para resolver por Jacobi
+def jacobi(A, b, tol=1e-10, max_iter=1000):
+    """
+    Resuelve sistemas de ecuaciones lineales Ax = b usando el metodo de Jacobi
+
+    Parametros:
+        A: Matriz de coeficientes.
+        b: Vector de terminos independientes.
+        tol: Tolerancia para la convergencia.
+        max_iter: Maximo numero de iteraciones.
+
+    Retorna:
+        x: Solucion aproximada.
+    """
+    n = len(b)
+    x = np.zeros(n)
+    x_new = np.zeros(n)
+
+
+    for k in range(max_iter):
+        for i in range(n):
+            x_new[i]= (b[i] - sum(A[i,j] * x[j] for j in range(n) if j != i))/A[i,i]
+
+        if np.linalg.norm(x_new - x) < tol:
+            print(f"Total de iteraciones necesitadas: {k}")
+            return x_new
+        
+        x = x_new.copy()
+    
+    print(f"No se pudo converger en {max_iter} iteraciones.")
+    return x
+
+
+#Metodo para resolver por Gauss-Seidel
+def gauss_seidel(A, b, tol=1e-10, max_iter=1000):
+
+    n = len(b)
+    x = np.zeros(n)
+
+    for k in range(max_iter):
+        x_prev = x.copy()
+
+        for i in range(n):
+            s = 0
+            for j in range(n):
+                if j != i:
+                    s += A[i, j] * x[j]
+            x[i] = (b[i] - s) / A[i, i]
+
+        if np.linalg.norm(x - x_prev) < tol:
+            print(f"Numero total de iteraciones necesitadas: {k}")
+            return x
+
+    print(f"No se pudo converger en {max_iter} iteraciones.")
+    return x
+
+
 
 
 
--- Pruebas/test.py
+++ Pruebas/test.py
@@ -261,4 +261,31 @@ if __name__ == "__main__":
     test_lu_decomposition()
 
 
+print("----------------------------------------")
+#Ejemplo Jacobi
+print("Jacobi")
+A = np.array([[3,-0.1,-0.2],[0.1,7,-0.3],[0.3,-0.2,10]], dtype=float)
+b = np.array([7.85,-19.3,71.4], dtype=float)
+
+
+ejemplo_jacobi =  jacobi(A,b)
+print(f"Solucion por gauss seidel: {ejemplo_jacobi}")
+
+
+print("----------------------------------------")
+# Ejemplo de Seidel
+print("Seidel")
+A = np.array([[3,-0.1,-0.2],[0.1,7,-0.3],[0.3,-0.2,10]], dtype=float)
+b = np.array([7.85,-19.3,71.4], dtype=float)
+
+ejemplo_gauss_seidel = gauss_seidel(A, b)
+print(f"Solucion por gauss seidel: {ejemplo_gauss_seidel}")
+
+# Mostrar resultados
+print(f"\nRespuestas Metodo Seidel:")
+print(f"x: {ejemplo_gauss_seidel[0]:.2f}%")
+print(f"y: {ejemplo_gauss_seidel[1]:.2f}%")
+print(f"z: {ejemplo_gauss_seidel[2]:.2f}%")
+
+
 
